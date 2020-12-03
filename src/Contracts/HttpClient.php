50c094e56681a90641435d47e4204c94867d60a8
atamiso <atamini@gmail.com>
2020-12-03 15:38:33 +0900

    HttpClientResponse Add Method

--- src/Contracts/HttpClient.php
+++ src/Contracts/HttpClient.php
@@ -10,8 +10,6 @@ use Illuminate\Http\Client\Response;
 
 abstract class HttpClient implements HttpClientInterface
 {
-    protected $logger;
-
     protected $driver;
 
     protected $client;
@@ -19,7 +17,6 @@ abstract class HttpClient implements HttpClientInterface
     public function __construct(DriverManager $manager)
     {
         $this->client = $manager->driver($this->driver);
-        $this->logger = resolve(HttpClientLogger::class);
     }
 
     public function __invoke($method, $url, $parameters = []): Response
@@ -29,7 +26,7 @@ abstract class HttpClient implements HttpClientInterface
 
     public function request($method, $url, $parameters = []): Response
     {
-        return tap($this->send($method, $url, $parameters), $this->logger->logging($method, $url, $parameters));
+        return $this->send($method, $url, $parameters);
     }
 
     abstract function send($method, $url, $parameters = []): Response;
